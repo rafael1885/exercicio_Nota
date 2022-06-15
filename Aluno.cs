25b495ea53353d69143c9d5edcf151e105325315
rafaelpin <skynet.repo@gmail.com>
2022-06-15 12:37:27 -0300

    Add project files.

---
+++ Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio
+{
+    class Aluno
+    {
+        string Nome { get; set; }
+        string Nota1 { get; set; }
+        string Nota2 { get; set  }
+
+        public  Aluno (string nome, int nota)
+        {
+
+        
+        
+        }
+    
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Nome dos Alunos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Nota1: ");
+                int nota = int.Parse(Console.ReadLine());
+                Console.Write("Nota2: ");
+                int nota2 = int.Parse(Console.ReadLine());
+            }
+        
+        
+        }
+    }
+}
