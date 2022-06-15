fe61ee25cb588881b0503660f2c36a89cbd14b0e
rafaelpin <skynet.repo@gmail.com>
2022-06-15 12:56:31 -0300

    exercico em andamento

--- Aluno.cs
+++ Aluno.cs
@@ -3,22 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace exercicio
 {
     class Aluno
     {
         string Nome { get; set; }
-        string Nota1 { get; set; }
-        string Nota2 { get; set  }
-
-        public  Aluno (string nome, int nota)
+        int Nota1 { get; set; }
+        int Nota2 { get; set; }
+        int Media { get; set; }
+        public  Aluno (string nome, int nota, int nota2, int media)
         {
-
-        
-        
+            Nome = nome;
+            Nota1 = nota;
+            Nota2 = nota2;
+            Media = media;
+            
+            
         }
     
-    
+        public void MediaNota()
+        {
+            Media += Nota1 + Nota2 / 2;
+
+        }
+        public override string ToString()
+        {
+            return Nome + ": ," + Media.ToString("F2", CultureInfo.InvariantCulture); 
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ namespace exercicio
             Console.WriteLine("Nome dos Alunos: ");
             int n = int.Parse(Console.ReadLine());
 
+            List<Aluno> list = new List<Aluno>();
+
             for (int i = 1; i < n; i++)
             {
                 Console.Write("Nome: ");
@@ -22,9 +24,19 @@ namespace exercicio
                 int nota = int.Parse(Console.ReadLine());
                 Console.Write("Nota2: ");
                 int nota2 = int.Parse(Console.ReadLine());
+                int media = int.Parse(Console.ReadLine());
+                list.Add(new Aluno(nome, nota, nota2, media));
+                Console.WriteLine();
+
             }
-        
-        
-        }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach (Aluno obj in list)
+            {
+                Console.WriteLine(obj);
+
+            }
     }
 }
